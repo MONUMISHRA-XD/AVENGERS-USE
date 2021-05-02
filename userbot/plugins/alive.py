9661fa83c89f21f8d389343e8c43b8a4178c577d
chris <toluwasechristopher45@gmail.com>
2021-05-02 08:56:07 +0000

    pylint: auto fixes

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,9 @@ from telethon import version
 from . import StartTime, catversion, get_readable_time, hmention, mention, reply_id
 
 CAT_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✴ MY ULTIMATE USERBOT IS WORKING SUCCESSFULLY ✴"
+CUSTOM_ALIVE_TEXT = (
+    Config.CUSTOM_ALIVE_TEXT or "✴ MY ULTIMATE USERBOT IS WORKING SUCCESSFULLY ✴"
+)
 EMOJI = Config.CUSTOM_ALIVE_EMOJI or "✴✴"
 
 
