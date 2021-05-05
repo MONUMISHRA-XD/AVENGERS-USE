2747ea59b4b307fb31f8d42213628f2f3cc219d0
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2021-05-05 22:23:11 +0000

    pylint: auto fixes

--- userbot/plugins/barcode.py
+++ userbot/plugins/barcode.py
@@ -9,7 +9,6 @@ from datetime import datetime
 
 import barcode
 from barcode.writer import ImageWriter
-
 from uniborg.util import lightning_cmd
 
 
@@ -65,9 +64,10 @@ async def _(event):
 
 
 from userbot import CMD_HELP
+
 CMD_HELP.update(
     {
-     "| | barcode | |": "`.barcode`\
+        "| | barcode | |": "`.barcode`\
 \n\nBarCode Generator\
 \n\n**Command** -  `.barcode` `(your text)``"
     }
