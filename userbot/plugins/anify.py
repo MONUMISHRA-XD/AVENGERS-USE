1dcc712ed679f62bde42f973dbe06ec145e4e140
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-05-07 03:15:51 +0100

    Update anify.py

--- userbot/plugins/anify.py
+++ userbot/plugins/anify.py
@@ -1,29 +1,24 @@
-# credits: @youngchris112 
-
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd
 
-
-@borg.on(admin_cmd(pattern="whatanime ?(.*)"))
+@bot.on(admin_cmd(pattern="whatanime ?(.*)"))
+@bot.on(sudo_cmd(pattern="whatanime ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("Reply to any user's media message.")
+        await edit_or_reply(event, "Reply to any user's media message.")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.media:
-        await event.edit("reply to media file")
+        await edit_or_reply(event, "reply to media file")
         return
     chat = "@WhatAnimeBot"
-    reply_message.sender
     if reply_message.sender.bot:
         await event.edit("Reply to actual users message.")
         return
-    cat = await event.edit("transforming this media")
+    cat = await edit_or_reply(event, "transforming this media")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -32,24 +27,25 @@ async def _(event):
             await event.client.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.reply("unblock @WhatAnimeBot and try again")
-            await cat.delete()
+            await cat.edit("unblock @WhatAnimeBot and try again")
             return
         if response.text.startswith("See next message."):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=394963134)
             )
             response = await response
-            cat = response.message.message
-            await event.edit(cat)
-
+            msg = response.message.message
+            await cat.edit(msg)
         else:
-            await event.edit("sorry, I couldnt find it")
+            await cat.edit("sorry, I couldnt find it")
+        await event.client.send_read_acknowledge(conv.chat_id)
 
 
 CMD_HELP.update(
     {
-        "whatanime": "`.whatanime` reply this to any media file\
-    \nUSAGE : transforming pic to vid.\
+        "recognize": "__**PLUGIN NAME :** whatanime__\
+    \n\n📌** CMD ➥** `.whatanime` reply this to any media file\
+    \n**USAGE   ➥  **Get an anime gif similar to that image .\
+    \nFind out information about similarity."
     }
 )
